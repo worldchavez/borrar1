4a60656f15755fd1b8c65ae7e63827c067e04af9
Allan <worldchavez@gmail.com>
2018-10-16 22:07:32 -0600

    creacion de la tabla asignacion de permisos

--- database/migrations/2018_10_17_034236_create_usuarios_table.php
+++ database/migrations/2018_10_17_034236_create_usuarios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsuarioTable extends Migration
+class CreateUsuariosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateUsuarioTable extends Migration
      */
     public function up()
     {
-        Schema::create('usuario', function (Blueprint $table) {
-            $table->increments('id');
+
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('idUsuario');
             $table->text('nombre')->default(null);
             $table->text('dpi')->default(null);
             $table->text('direccion')->default(null);
@@ -37,6 +38,6 @@ class CreateUsuarioTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('usuario');
+        Schema::dropIfExists('usuarios');
     }
 }
---
+++ database/migrations/2018_10_17_034949_create_asignaciondepermisos_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAsignaciondepermisosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asignaciondepermisos', function (Blueprint $table) {
+            $table->increments('idAsignaciondePermisos');
+            $table->boolean('codigosucursal')->default(null);
+            $table->boolean('codigousuario')->default(null);
+            $table->boolean('caja')->default(null);
+            $table->boolean('pos')->default(null);
+            $table->boolean('cxc')->default(null);
+            $table->boolean('configuracion')->default(null);
+            $table->boolean('anularventa')->default(null);
+            $table->boolean('precioA')->default(null);
+            $table->boolean('precioB')->default(null);
+            $table->boolean('precioC')->default(null);
+            $table->boolean('precioD')->default(null);
+            $table->boolean('abajoCosto')->default(null);
+            $table->boolean('reporte')->default(null);
+            $table->boolean('bajarprecio')->default(null);
+            $table->boolean('modificarventa')->default(null);
+            $table->boolean('producto')->default(null);
+            $table->boolean('compras')->default(null);
+            $table->boolean('cxp')->default(null);
+            $table->boolean('ventasrealizadas')->default(null);
+            $table->boolean('entrada')->default(null);
+            $table->boolean('salida')->default(null);
+            $table->boolean('activo')->default(null);
+            $table->boolean('vercosto')->default(null);
+            $table->boolean('cliente')->default(null);
+
+            $table->tinyInteger('estado')->default(null);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asignaciondepermisos');
+    }
+}
